0b535b9d942c223b8ca0ffd1a46ad1eae3f95e4d
Tomyoza <120688111+Tomyoza@users.noreply.github.com>
2026-09-08 12:47:06 +0800

    Add myportfolio project entry and update page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import {Inter} from "next/font/google";
 const inter = Inter({subsets: ["latin"]})
 
 export const metadata: Metadata = {
-  title: "First-portfolio",
+  title: "Tomoki Yoza | Software Engineer",
   description: "Dev portfolio by Tomoki Yoza",
 };
 
